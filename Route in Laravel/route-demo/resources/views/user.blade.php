b72c31cf3616ac1339de6af18ce6d16088c8a675
Ninh Văn Tuấn Đạt <86716708+Ninh-Dat@users.noreply.github.com>
2022-02-27 20:21:33 +0700

    route laravel

---
+++ Route in Laravel/route-demo/resources/views/user.blade.php
@@ -0,0 +1,26 @@
+{{--<!doctype html>--}}
+{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
+{{--<head>--}}
+{{--    <meta charset="utf-8">--}}
+{{--    <meta name="viewport" content="width=device-width, initial-scale=1">--}}
+
+{{--    <title>Laravel Route</title>--}}
+{{--</head>--}}
+{{--<body>--}}
+{{--<h1>Welcome <?= $name ?>!</h1>--}}
+{{--</body>--}}
+{{--</html>--}}
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Tên: <?= $name ?>!</h1>
+<h1>Tuổi: <?= $age ?></h1>
+</body>
+</html>
